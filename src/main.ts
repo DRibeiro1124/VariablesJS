3fd2a590b87a73bfe4d897257f0e899b1b779288
Dino Ribeiro <DRibeiro1124@gmail.com>
2023-06-07 19:12:52 -0400

    completed the first 3 exercises

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
 import './style.css'
+
+// Practice Creating Variables
+const numberOfCupsOfCoffee = 1
+const fullName = 'Dino Ribeiro'
+console.log(
+  `Hello! My name is ${fullName} and currently I am just drinking ${numberOfCupsOfCoffee} cup of coffee every morning.`
+)
+
+//Create an object variable
+
+const aboutMe = {
+  fullName: 'Dino Ribeiro',
+  luckyNumber: 11,
+  favoriteMovies: [
+    'Saving Private Ryan',
+    'Apollo 13',
+    'Anchorman, the legend of Ron Burgandy',
+  ],
+}
+
+//Practice getting input from user
+
+const userName = window.prompt('Please enter your name:')
+console.log(`Hello, ${userName}!`)
