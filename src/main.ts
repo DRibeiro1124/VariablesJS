071be692f910c3facaf0121c30f8c8c92685d0d8
Dino Ribeiro <DRibeiro1124@gmail.com>
2023-06-07 20:14:37 -0400

    explorer mode complete

--- src/main.ts
+++ src/main.ts
@@ -33,3 +33,35 @@ const product = firstOperand * secondOperand
 const remainder = firstOperand % secondOperand
 
 console.log(`The sum of ${firstOperand} and ${secondOperand} is ${sum}`)
+
+//Using arrays
+const numbers = [6, 7, 10, 11, 24, 7]
+
+let smallest = numbers[0]
+let largest = numbers[0]
+let arraySum = 0
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < smallest) {
+    smallest = numbers[i]
+  }
+  if (numbers[i] > largest) {
+    largest = numbers[i]
+  }
+  arraySum += numbers[i]
+}
+
+const average = arraySum / numbers.length
+
+// Create an object called stats
+const stats = {
+  smallest: smallest,
+  largest: largest,
+  sum: arraySum,
+  average: average,
+}
+
+console.log(`Smallest number in the array: ${stats.smallest}`)
+console.log(`Largest number in the array: ${stats.largest}`)
+console.log(`Sum of all numbers in the array: ${stats.sum}`)
+console.log(`Average of all numbers in the array: ${stats.average}`)
